c23de0af156d176b2dde3afae69da6a50eb37e4e
Alex Morehead <alex.morehead@gmail.com>
2022-07-24 12:29:02 -0500

    Add files via upload

---
+++ capstone_project_scripts/random_minerl_agent.py
@@ -0,0 +1,22 @@
+# import packages
+import gym
+import minerl
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+# set up OpenAI Gym for MineRL's `BASALT - Find Cave v0 challenge`
+env = gym.make('MineRLBasaltFindCave-v0')
+
+# note: this command will launch the MineRL environment, which takes time. Be patient!
+obs = env.reset()
+
+# take actions until either the agent dies or the three-minute timer runs out
+done = False
+while not done:
+    # Take a random action
+    action = env.action_space.sample()
+    # In BASALT environments, sending ESC action will end the episode
+    # Lets not do that
+    action["ESC"] = 0
+    obs, reward, done, _ = env.step(action)
+    env.render()
